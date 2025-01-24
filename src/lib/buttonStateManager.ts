4929682fd784eafa44a444b96b80295af23d1006
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-01-24 20:40:58 +0100

    create module for button manager

---
+++ src/lib/buttonStateManager.ts
@@ -0,0 +1,57 @@
+type ButtonState = 'default' | 'loading';
+
+type ButtonStateValues = Record<
+  ButtonState,
+  {
+    text: string;
+    disabled: boolean;
+  }
+>;
+
+const DEFAULT_STATE_VALUES: ButtonStateValues = Object.freeze({
+  default: {
+    text: 'Submit',
+    disabled: false,
+  },
+  loading: {
+    text: 'Loading...',
+    disabled: true,
+  },
+});
+
+export class Button {
+  private btn: HTMLButtonElement;
+  private stateValues: ButtonStateValues = DEFAULT_STATE_VALUES;
+
+  constructor(btn: HTMLButtonElement, loadingText?: string) {
+    this.btn = btn;
+    this.stateValues.default.text = this.text ?? this.stateValues.default.text;
+    this.stateValues.loading.text = loadingText ?? this.stateValues.loading.text;
+
+    console.log(this.stateValues);
+
+    console.log(this.stateValues);
+  }
+
+  private get text() {
+    return this.btn.textContent ?? '';
+  }
+
+  private set text(value: string) {
+    this.btn.textContent = value.trim();
+  }
+
+  private disable() {
+    this.btn.setAttribute('disabled', 'true');
+  }
+
+  private enable() {
+    this.btn.removeAttribute('disabled');
+  }
+
+  setState(state: ButtonState) {
+    const newState = this.stateValues[state];
+    this.text = newState.text;
+    newState.disabled ? this.disable() : this.enable();
+  }
+}
