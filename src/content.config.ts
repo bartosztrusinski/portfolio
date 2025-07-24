e106377abb7c331d06b439d27334e39d39bcead5
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-07-24 17:26:23 +0200

    make tech stack type-safe

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,6 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
+import { technologyKeys, type TechnologyKey } from '@/data/technologies';
 
 const projects = defineCollection({
   loader: glob({ pattern: '**/*.json', base: './src/data/projects' }),
@@ -9,7 +10,7 @@ const projects = defineCollection({
       description: z.string(),
       logo: image().optional(),
       thumbnail: image().optional(),
-      techStack: z.array(z.string()),
+      techStack: z.array(z.enum(technologyKeys as [TechnologyKey, ...TechnologyKey[]])),
       features: z.array(z.string()),
       url: z.string().url().optional(),
       repo: z.string().url(),
