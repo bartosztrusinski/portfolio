26ac67c08d45edc4e3f5bf4fdadc425d535721ea
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-07-24 17:30:59 +0200

    move skills to data file

---
+++ src/data/skills.ts
@@ -0,0 +1,21 @@
+import type { TechnologyKey } from '@/data/technologies';
+
+type Skill = {
+  title: string;
+  technologyKeys: TechnologyKey[];
+};
+
+export const skills: Skill[] = [
+  {
+    title: 'Front-End',
+    technologyKeys: ['react', 'javascript', 'typescript', 'next', 'astro', 'tailwind', 'shadcnui'],
+  },
+  {
+    title: 'Back-End',
+    technologyKeys: ['node', 'express', 'prisma', 'drizzle', 'postgres', 'mongodb'],
+  },
+  {
+    title: 'Tools',
+    technologyKeys: ['git', 'webpack', 'zod'],
+  },
+];
